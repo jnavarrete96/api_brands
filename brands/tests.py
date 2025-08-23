c44f8464f766048f575aa305371a7948ded671ef
Juan Pablo Navarrete <navarrete0725@gmail.com>
2025-08-23 17:53:32 -0500

    agregar prueba unitaria

--- brands/tests.py
+++ brands/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from brands.models import Brand, Owner
 
-# Create your tests here.
+class BrandListViewTest(APITestCase):
+    def setUp(self):
+        owner = Owner.objects.create(name="Juan Navarrete")
+        Brand.objects.create(name="Marca 1", owner=owner)
+        Brand.objects.create(name="Marca 2", owner=owner)
+
+    def test_list_all_brands(self):
+        url = reverse('brand-list')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data["data"]), 2)
+        self.assertEqual(response.data["data"][0]["owner"]["name"], "Juan Navarrete")
